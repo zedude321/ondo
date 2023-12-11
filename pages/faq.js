57683376b352e43dfdc46c921a697496bb7e5a18
Zunbileg Mungundush <zunbilegmungundush@R4izzZzz1s-MacBook-Air.local>
2023-12-11 08:20:56 +0800

    started on faq

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,16 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+
 export default function FAQ () {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header />
+
+            <div>
+                
+            </div>
+
+            <Footer />
+        </>
     )
 }
